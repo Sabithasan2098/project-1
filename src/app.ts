c945257c04c4c4f6361cfc7a1120cf13b220bc0e
Sabithasan2098 <sabithasan2098@gmail.com>
2024-05-16 12:06:51 +0600

    error handeling system

--- src/app.ts
+++ src/app.ts
@@ -40,14 +40,47 @@ courseRouter.post("/createCourse", (req: Request, res: Response) => {
 // --------------------------------------------//
 
 // simple get and post request------------------->
-app.get("/", logger, (req: Request, res: Response) => {
-  console.log(req.query);
-  res.send("Hello developers from the world!");
-});
 
 app.post("/", logger, (req: Request, res: Response) => {
   console.log(req.body);
   res.json("Post data successfully!");
 });
 // --------------------------------//
+
+// use async and try catch to run the server when its face an error--------------->
+app.get(
+  "/",
+  logger,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      console.log(req.query);
+      res.send("Hello developers from the world!");
+    } catch (error) {
+      next(error);
+      // console.log(error);
+      // res.status(400).json({
+      //   success: false,
+      //   message: "failed to get data",
+      // });
+    }
+  }
+);
+// -----------------------------------------------------------//
+
+// routing error------------------------------->
+app.all("*", (req: Request, res: Response) => {
+  res.status(400).json({
+    success: false,
+    message: "Routing path error",
+  });
+});
+
+// global error handle--------------------------->
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(400).json({
+    success: false,
+    message: "Something went wrong",
+  });
+});
+// ----------------------------------------//
 export default app;

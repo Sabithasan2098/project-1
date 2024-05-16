a369e09ec45f57b25e855142cd3e611d3911cace
Sabithasan2098 <sabithasan2098@gmail.com>
2024-05-16 10:46:19 +0600

    make  a middleware

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,25 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 const app = express();
 // json data perser
 app.use(express.json());
 // text data perser
 app.use(express.text());
 
-app.get("/", (req:Request, res:Response) => {
+// make a middleware-------------------->
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
+  next();
+};
+// --------------------//
+
+app.get("/", logger, (req: Request, res: Response) => {
+  console.log(req.query);
   res.send("Hello developers from the world!");
 });
 
-
-app.post("/", (req:Request, res:Response) => {
+app.post("/", logger, (req: Request, res: Response) => {
   console.log(req.body);
   res.json("Post data successfully!");
-})
+});
 
 export default app;

d8e30fc7e2fc67810561036a55274a589bf81ddf
Sabithasan2098 <sabithasan2098@gmail.com>
2024-05-16 11:21:38 +0600

    create router use express.router

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,34 @@ const logger = (req: Request, res: Response, next: NextFunction) => {
 };
 // --------------------//
 
+// create a users router use express.router--------------------->
+const userRouter = express.Router();
+app.use("/api/v1/users", userRouter);
+userRouter.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+  res.json({
+    success: true,
+    message: "user create successfully",
+    data: user,
+  });
+});
+// ----------------------------------//
+// create a course router use express.router-------->
+const courseRouter = express.Router();
+app.use("/api/v1/course", courseRouter);
+courseRouter.post("/createCourse", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: true,
+    message: "course create successfully",
+    data: course,
+  });
+});
+// --------------------------------------------//
+
+// simple get and post request------------------->
 app.get("/", logger, (req: Request, res: Response) => {
   console.log(req.query);
   res.send("Hello developers from the world!");
@@ -21,5 +49,5 @@ app.post("/", logger, (req: Request, res: Response) => {
   console.log(req.body);
   res.json("Post data successfully!");
 });
-
+// --------------------------------//
 export default app;
